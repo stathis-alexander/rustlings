06cf472e6104ac5288f0e25717d2d43af3e52a96
Alexander Stathis <stathis.alexanderj@gmail.com>
2025-08-02 12:37:42 -0400

    rustlings 1 - 3

---
+++ exercises/02_functions/functions1.rs
@@ -0,0 +1,11 @@
+// TODO: Add some function with the name `call_me` without arguments or a return value.
+
+fn call_me() {
+  println!("Ring! Call number 1");
+  println!("Ring! Call number 2");
+  println!("Ring! Call number 3");
+}
+
+fn main() {
+  call_me(); // Don't change this line
+}
